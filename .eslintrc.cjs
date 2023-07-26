142d6c9730cff4467876cec6d3f3c2e570d018cb
peacepiece7 <peacepiece7@naver.com>
2023-07-26 15:30:56 +0900

    :hammer: : husky로 pre commit 스크립트 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:react/recommended', // self closing tag룰 적용하기 위해서 "eslint-plugin-react" 추가 설치
+    'plugin:react/recommended' // self closing tag룰 적용하기 위해서 "eslint-plugin-react" 추가 설치
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,15 +14,15 @@ module.exports = {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     quotes: ['error', 'single'],
     'no-duplicate-imports': 'error',
-    'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
     'no-unused-vars': 'error',
     'no-multiple-empty-lines': 'error',
     'react/self-closing-comp': [
       'error',
       {
         component: true,
-        html: true,
-      },
-    ],
-  },
+        html: true
+      }
+    ]
+  }
 }
