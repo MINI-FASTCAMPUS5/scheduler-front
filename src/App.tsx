3debb0642480803cfc89e1004e71f75533ec841d
peacepiece7 <peacepiece7@naver.com>
2023-07-27 19:11:06 +0900

    :art: : 로그인 context 구조 추가, layout 여러 페이지에 따라 다르게 적용할 수 있도록 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import CalendarLayout from '@/components/CalendarLayout'
+import CalendarLayout from '@/components/layout/CalendarLayout'
 import CalendarPage from '@/pages/Calendar'
 import Sample from '@/pages/Sample'
 import '@/App.css'
 import Optimistic from './pages/Optimistic'
+import UserProvider from './context/UserProvider'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/login' element={<div>로그인 페이지 레이아웃</div>} />
-      <Route path='/' element={<CalendarLayout />}>
-        <Route path='/' element={<CalendarPage />} />
-        <Route path='/sample' element={<Sample />} />
-        <Route path='/sample/optimistic' element={<Optimistic />} />
-      </Route>
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path='/login' element={<div>로그인 페이지 레이아웃</div>} />
+        <Route path='/' element={<CalendarLayout />}>
+          <Route path='/' element={<CalendarPage />} />
+          <Route path='/sample' element={<Sample />} />
+          <Route path='/sample/optimistic' element={<Optimistic />} />
+        </Route>
+      </Routes>
+    </UserProvider>
   )
 }
 
--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,24 +1,4 @@
 import React from 'react'
 export default function CalendarPage() {
-  return (
-    <div className='flex-1 bg-orange-[rgba(213, 171, 123, 0.9)]'>
-      CalendarPage
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-      <h1 className='text-9xl'>TEST</h1>
-    </div>
-  )
+  return <div className='flex-1 bg-orange-[rgba(213, 171, 123, 0.9)]'>CalendarPage</div>
 }
