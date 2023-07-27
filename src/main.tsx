bb83b314fe27fd6391b7cd2e818ea4344297e725
peacepiece7 <peacepiece7@naver.com>
2023-07-28 08:47:29 +0900

    :ambulance: user provider 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import UserProvider from '@/context/UserProvider'
+
 import App from '@/App.tsx'
 import '@/index.css'
 
@@ -9,8 +11,10 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
   </QueryClientProvider>
 )
