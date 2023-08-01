6374a749d787e0fc0621de20c0c3c091a47db293
peacepiece7 <peacepiece7@naver.com>
2023-08-01 17:42:53 +0900

    :sparkles: : #22 데일리 캘린더 모달 기능 추가 & 로딩 애니메이션 변경

--- src/components/calendar/CalendarFrame.tsx
+++ src/components/calendar/CalendarFrame.tsx
@@ -2,6 +2,7 @@ import useSchedule from '@/hooks/schedule'
 import React from 'react'
 import MonthlyCalendar from '@/components/calendar/MonthlyCalendar'
 import CalendarSwiper from '@/components/calendar/CalendarSwiper'
+import CheerUpLoading from '../ui/cheerUpLoading'
 
 export default function CalendarFrame() {
   const { isFetching } = useSchedule()
@@ -11,11 +12,12 @@ export default function CalendarFrame() {
       <CalendarSwiper />
       {/* 임시 fetch loading 태그입니다. */}
       {isFetching && (
-        <div className='fixed flex w-full h-full left-0 top-0 bg-purple-200 z-[99999] justify-center items-center opacity-60 overflow-hidden'>
-          <div className='text-black  text-7xl font-extrabold'>
-            schedule data를 가져오고 있습니다!
+        <div className='fixed flex flex-col w-full h-full left-0 top-0 bg-purple-50 z-[99999] justify-center items-center opacity-90 overflow-hidden'>
+          <div className='text-black  text-7xl font-extrabold opacity-80 mb-12'>
+            데이터를 가져오고 있습니다!
           </div>
-          <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-100  opacity-75' />
+          <CheerUpLoading />
+          <span className='aanimate-ping absolute inline-flex h-full w-full rounded-full bg-purple-100  opacity-10' />
           {/* <span className='relative inline-flex rounded-full h-3 w-3 bg-orange-500 ' /> */}
         </div>
       )}
