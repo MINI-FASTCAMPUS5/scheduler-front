dc5f13f9fc174b834d3d3fd1dd702669f6b2e6fd
DICEPT <dicepted@gmail.com>
2023-08-02 13:15:22 +0900

    :lipstick: ApprovalListItem 수정/취소 버튼 스타일 변경

--- src/pages/Manager/Approval/ApprovalListItem.tsx
+++ src/pages/Manager/Approval/ApprovalListItem.tsx
@@ -6,8 +6,8 @@ export default function ApprovalListItem() {
       <div className='flex w-3/5 mt-auto mb-auto text-base font-bold'>BTS 서울 공연</div>
       <div className='flex w-1/5 mt-auto mb-auto text-base'>2023-08-20</div>
       <div className='flex w-1/5 mt-auto mb-auto gap-2 justify-end'>
-        <button className='flex w-16 border border-box-line-color rounded-lg justify-center text-sm'>수정</button>
-        <button className='flex w-16 border border-box-line-color rounded-lg justify-center text-sm'>취소</button>
+        <button className='flex w-16 border text-main-color border-main-color rounded-lg justify-center text-sm'>수정</button>
+        <button className='flex w-16 border text-point-color border-point-color rounded-lg justify-center text-sm'>취소</button>
       </div>
     </div>
   )
