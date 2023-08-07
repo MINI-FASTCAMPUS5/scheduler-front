1165b950ccd13e1f53a6db808eef036a106cff0a
0299bang <0299bang@gmail.com>
2023-08-07 19:43:29 +0900

    :sparkles: 사용자 회원정보수정 API 연결(수정중)

--- src/pages/User/Edit.tsx
+++ src/pages/User/Edit.tsx
@@ -1,94 +1,107 @@
-import useUser from '@/hooks/user'
+import React, { useEffect, useState } from 'react'
 import Button from '@/components/ui/Button'
-import React, { useEffect } from 'react'
+import { getProfileInfo, updateUserProfile } from '@/api/userEdit'
+//uploadProfileImage
+import { useCookies } from 'react-cookie'
 
 export default function Edit() {
-  // useUser 훅으로부터 필요한 상태와 함수들을 가져온다.
-  const { getUserInfo, loggedIn } = useUser()
+  const [cookies] = useCookies(['AccessToken'])
 
-  // 사용자의 프로필 이미지, 이름, 이메일, 새로운 비밀번호에 대한 상태들을 정의한다.
-  const [profileImage, setProfileImage] = React.useState<string>('')
-  const [name, setName] = React.useState<string>('')
-  const [email, setEmail] = React.useState<string>('')
-  const [newPassword, setNewPassword] = React.useState<string>('')
-  const [confirmPassword, setConfirmPassword] = React.useState<string>('')
-
-  // useUser 훅으로부터 가져온 사용자 정보를 user 변수에 저장한다.
-  const user = getUserInfo()
+  //const [profileImage, setProfileImage] = useState<File | null>(null)
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [newPassword, setNewPassword] = useState<string>('')
+  const [confirmPassword, setConfirmPassword] = useState<string>('')
 
   useEffect(() => {
-    // 사용자가 로그인한 상태라면, useUser 훅으로부터 가져온 정보를 사용하여 상태를 업데이트한다.
-    if (loggedIn) {
-      setProfileImage(() => (user?.profileImage ? user?.profileImage : ''))
-      setName(() => (user?.fullName ? user?.fullName : ''))
-      setEmail(() => (user?.email ? user?.email : ''))
+    async function fetchProfileInfo() {
+      try {
+        const profileInfo = await getProfileInfo(cookies.AccessToken)
+        //setProfileImage(profileInfo.data.profileImage)
+        setName(profileInfo.data.fullName)
+        setEmail(profileInfo.data.email)
+      } catch (error) {
+        console.error('프로필 정보 가져오기 오류:', error)
+      }
     }
-  }, [loggedIn, user?.email, user?.fullName, user?.profileImage])
 
-  // 이름 변경 이벤트 핸들러
+    fetchProfileInfo()
+  }, [cookies.AccessToken])
+
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value)
   }
 
-  // 새로운 비밀번호 입력 이벤트 핸들러
   const handleNewPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewPassword(e.target.value)
   }
 
-  // 비밀번호 확인 입력 이벤트 핸들러
   const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setConfirmPassword(e.target.value)
   }
 
-  // "취소" 버튼 클릭 핸들러
   const handleCancel = () => {
-    // 취소 버튼 클릭 시 동작할 내용을 작성
+    // 취소 버튼 클릭 시 동작할 내용 추가
   }
 
-  // "수정 완료" 버튼 클릭 핸들러
-  const handleEditComplete = () => {
-    // 수정 완료 버튼 클릭 시 동작할 내용을 작성
+  const handleEditComplete = async () => {
+    try {
+      if (newPassword !== confirmPassword) {
+        console.error('비밀번호가 일치하지 않습니다.')
+        return
+      }
+
+      await updateUserProfile(cookies.AccessToken, name, newPassword)
+
+      // if (profileImage !== null) {
+      //   const imageUrl = URL.createObjectURL(profileImage)
+      //   await uploadProfileImage(cookies.AccessToken, profileImage)
+      //   setProfileImage(imageUrl)
+      // }
+
+      // 수정 완료 후 추가 동작
+    } catch (error) {
+      console.error('에러 발생:', error)
+    }
   }
 
   return (
     <div className='flex flex-col ml-4 p-4 h-screen'>
-      {/* 타이틀 */}
       <h1 className='text-3xl font-bold border-b-2 pb-2 mb-8'>회원정보 수정</h1>
 
       <div className='flex items-center'>
-        {/* 프로필 이미지 영역 */}
         <div className='w-24 h-24 mb-10 flex items-center justify-center overflow-hidden rounded-full'>
-          <img src={profileImage} alt='Profile Image' />
+          {/* {profileImage !== null ? (
+            <img src={profileImage} alt='Profile Image' />
+          ) : (
+            <div>프로필 이미지 없음</div>
+          )} */}
         </div>
-        {/* "수정" 버튼 */}
         <Button
           text='수정'
           size='sm'
           className='bg-point text-white rounded ml-8'
           onClick={() => {
-            // "수정" 버튼을 클릭하면 숨겨진 파일 선택 창을 트리거한다.
             document.getElementById('profile-image-input')?.click()
           }}
         />
-        {/* 숨겨진 파일 입력 창 */}
-        <input
+        {/* <input
           type='file'
           id='profile-image-input'
           accept='image/*'
           style={{ display: 'none' }}
           onChange={(e) => {
-            // 여기서 선택된 파일을 처리합니다.
             if (e.target.files && e.target.files[0]) {
+              const selectedFile = e.target.files[0] as File
               const fileReader = new FileReader()
               fileReader.onload = (event) => {
                 const uploadedImageUrl = event.target?.result as string
-                // 선택한 이미지로 프로필 이미지를 설정합니다.
                 setProfileImage(uploadedImageUrl)
               }
-              fileReader.readAsDataURL(e.target.files[0])
+              fileReader.readAsDataURL(selectedFile)
             }
           }}
-        />
+        /> */}
       </div>
 
       {/* 그리드로 수정된 영역 */}
@@ -182,16 +195,13 @@ export default function Edit() {
         </div>
       </div>
 
-      {/* 버튼들을 수정 */}
       <div className='flex'>
-        {/* "취소" 버튼 */}
         <Button
           text='취소'
           size='lg'
           onClick={handleCancel}
           className='px-4 py-2 mr-4 bg-point text-white rounded'
         />
-        {/* "수정 완료" 버튼 */}
         <Button
           text='수정 완료'
           size='lg'
