8183c4c571db4003badb69ba3b1532e334bbf2d2
DICEPT <dicepted@gmail.com>
2023-08-07 20:30:21 +0900

    :lipstick: :  행사 등록하기, 신청자 승인/ 취소 관리 버튼 위치 변경

--- src/pages/Manager/ManagerDashboard/index.tsx
+++ src/pages/Manager/ManagerDashboard/index.tsx
@@ -1,7 +1,7 @@
 import useUser from '@/hooks/user'
 import React from 'react'
-import AddEventList from './AddEventList'
-import EventStatusCard from './EventStatusCard'
+import AddEventList from '@/components/dashboard/AddEventList'
+import EventStatusCard from '@/components/dashboard/EventStatusCard'
 import userDefaultImg from '/mock_image/user_default.png'
 
 export default function ManagerDashboardPage() {
@@ -10,17 +10,18 @@ export default function ManagerDashboardPage() {
   const defaultprofileImage = userDefaultImg
 
   return (
-    <div className='flex flex-col m-2 ml-8 h-98vh overflow-hidden'>
+    <div className='flex flex-col pt-2 pb-2 ml-8 mr-8 h-[100vh]'>
       <div className='flex flex-col font-bold text-4xl mt-10'>
         매니저 대시보드
         <div className='border-b-2 border-boxline mb-6 mt-3' />
       </div>
-      <div className='flex w-full h-[900px] ml-4'>
-        <div className='flex border w-2/5 mr-10 border-boxline overflow-hidden  rounded-3xl relative'>
-          <div className='flex flex-col m-auto absolute left-0 right-0 top-0 bottom-0 z-20'>
+      <div className='flex w-full h-full'>
+        <div className='flex border w-2/5 mr-10 border-boxline overflow-hidden justify-center pt-auto pb-auto rounded-3xl relative'>
+          {/* 프로필 이미지 */}
+          <div className='flex flex-col absolute z-20 m-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
             {user.profileImage ? (
               <img
-                className='flex ml-auto mr-auto mt-80 w-32 h-32 rounded-full aspect-square object-cover border border-boxline'
+                className='flex ml-auto mr-auto w-32 h-32 rounded-full aspect-square object-cover border border-boxline'
                 src={user.profileImage}
               />
             ) : (
@@ -36,6 +37,7 @@ export default function ManagerDashboardPage() {
             className='w-full h-full bg-white bg-opacity-10 absolute z-10'
             style={{ backdropFilter: 'blur(30px)' }}
           />
+          {/* 프로필 배경 이미지 */}
           {user.profileImage ? (
             <div
               className='w-full h-full bg-center bg-no-repeat bg-cover'
@@ -48,17 +50,9 @@ export default function ManagerDashboardPage() {
             />
           )}
         </div>
-        <div className='flex flex-col ml-5 w-3/6'>
+        <div className='flex flex-col ml-5 w-3/6 h-[100%]'>
           <EventStatusCard />
           <AddEventList />
-          <div className='flex w-full gap-5 mt-5 mb-12 pl-10 pr-10'>
-            <button className='flex h-20 w-full pt-6 text-white text-2xl font-bold justify-center bg-main rounded-3xl transition hover:bg-[#411b92]'>
-              행사 등록하기
-            </button>
-            <button className='flex h-20 w-full pt-6 text-white text-2xl font-bold justify-center bg-wait rounded-3xl transition hover:bg-[#ef8a0f]'>
-              신청자 승인/취소 관리
-            </button>
-          </div>
         </div>
       </div>
     </div>
