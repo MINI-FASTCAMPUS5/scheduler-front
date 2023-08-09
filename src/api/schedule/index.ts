49e11a19ac51efd7d3f14cc695669fd50e71b2b5
peacepiece7 <peacepiece7@naver.com>
2023-08-09 11:17:02 +0900

    fix : 검색 API null이 반환되는 케이스 예외 처리

--- src/api/schedule/index.ts
+++ src/api/schedule/index.ts
@@ -50,15 +50,8 @@ export const fetchSchedule = async ({
       }
     })
 
-    // 데이터가 없을 경우 서버측에서 null이 들어간 배열을 반환함
-    if (!res.data.schedulerAdmin[0]) {
-      return {
-        schedule: [],
-        reservedList: reservedList
-      }
-    }
-
     const schedule = res.data.schedulerAdmin
+      .filter((s) => s)
       .map((s) => {
         return {
           id: s.id, // post id PK가 안와서 임시로 만듬
