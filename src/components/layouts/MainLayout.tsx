fad4827684c604d83fcc89163081f1a6af19b6a5
DICEPT <dicepted@gmail.com>
2023-08-10 00:58:51 +0900

    :lipstick: :  메인 레이아웃 타이틀 추가 및 배경 애니메이션 레이아웃 조절

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -25,11 +25,16 @@ export default function MainLayout() {
   }, [getUserInfo, navigate, loggedIn, location.pathname, loading])
 
   return (
-    <main className='relative flex h-[100vh] w-[100vw]'>
-      <div className='absolute flex max-h-[400px] border-4 p-4 rounded-xl z-[999] m-auto top-0 right-48 bottom-0'>
-        <Outlet />
-      </div>
+    <div className='h-[100vh] w-[100vw]'>
+      <div className=' absolute flex text-[50px] z-40 top-5 left-8'><img src='/YeonganIdolLogoWhite.svg' className='w-[600px]'/></div>
+      <div className=' absolute flex text-[16px] z-40 bottom-9 left-8 text-white opacity-50'>© 2023. 12 GROUP All rights reserved.</div>
+      <div className=' absolute flex text-[10px] z-40 bottom-5 left-8 text-white opacity-50'>Illustrated by Chulmin Park & Source Zustand</div>
+      <main className='absolute flex h-[100vh] w-[100vw] p-auto'>
+        <div className=' flex w-[480px] bg-white p-10 rounded-[20px] z-[999] m-auto mr-[300px] shadow-xl'>
+          <Outlet />
+        </div>
+      </main>
       <Scene />
-    </main>
+    </div>
   )
 }
